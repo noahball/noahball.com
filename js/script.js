c5a725a27399583438f9fb12f0a82ca52373c3d2
Noah <noah@noahball.com>
2021-10-03 21:24:23 +1300

    Contact form (functioning)

--- js/script.js
+++ js/script.js
@@ -1,2 +1,15 @@
 // Fade in effect (jQuery)
 $('section').fadeIn('slow');
+
+// Contact Form Success
+const queryString = window.location.search;
+if (queryString.includes('?result=success') == true) {
+    $('#contact-submit').hide();
+    $('#contact-response').show();
+    $('#contact-response').text('The carrier pigeons were dispatched successfully!');
+    $('html,body').animate({scrollTop: document.body.scrollHeight},"fast");
+} else if (queryString.includes('?result=error') == true) {
+    $('#contact-submit').hide();
+    $('#contact-response').show();
+    $('#contact-response').text('Something went wrong.')
+}
